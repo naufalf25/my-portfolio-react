58d72d62dc40ce1c7490641b75a0b387c72f8b6f
naufalf25 <opal.farras@gmail.com>
2023-01-25 13:47:42 +0700

    repair some UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   ]
 
   return (
-    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
+    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed z-10'>
       <div>
         <h1 className='text-5xl font-signature ml-2'>Naufal</h1>
       </div>
@@ -46,7 +46,7 @@ const Navbar = () => {
       <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-30 text-gray-500 md:hidden'>
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
-      <ul className={'flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 z-20 duration-700' + (nav ? ' mt-0' : ' -mt-[1000px]')}>
+      <ul className={'flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 z-20 duration-700' + (nav ? ' mt-0' : ' -mt-[1500px]')}>
         {links.map(({ id, link }) => (
           <li key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>
             <Link onClick={() => setNav(!nav)} to={link} smooth duration={500}>{link}</Link>
